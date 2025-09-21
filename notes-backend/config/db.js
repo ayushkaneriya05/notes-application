ea9c3104baf29aafd6e54d55d780deea41ca2fce
Ayush Kaneriya <ayushkaneriya05@gmail.com>
2025-09-21 19:19:19 +0530

    update db connection

--- notes-backend/config/db.js
+++ notes-backend/config/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connection.db.notesdb().command({ ping: 1 });
     console.log("✅ MongoDB connected");
   } catch (err) {
     console.error("❌ DB Connection failed", err);
